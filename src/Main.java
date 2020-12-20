263124514a8d2ede4e715266eee5de942009ceae
Miguel Macedo <miguelmarquesmacedo@gmail.com>
2020-12-20 17:14:26 +0000

    backbone v2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import java.io.*;
+import java.net.*;
+
 public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = InetAddress.getLocalHost();
+        Node clientNode = new Node(address, 11111);
+        clientNode.run();
+        //Node serverNode = new Node(address, 22222);
+
+
+
+    }
+
 }
--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,26 @@
-public class Node {
+import java.io.*;
+import java.net.*;
+
+public class Node implements Runnable{
+    private InetAddress address;
+    private int port;
+    private Socket socket;
+
+
+    Node(InetAddress address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        while(true)
+            System.out.println("Pa");
+    }
+
+    public void startConnection() {
+        
+    }
 }
+
+
